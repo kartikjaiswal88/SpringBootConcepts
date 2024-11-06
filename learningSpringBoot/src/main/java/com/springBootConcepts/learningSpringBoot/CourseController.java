ea72ee59f474ff652927e0852dc43c5ee6b57ffa
Kartik Jaiswal <kartikjaiswal038@gmail.com>
2024-11-06 09:59:58 +0530

    Understanding springInitializer, starterProject, autoConfiguration, devtools

--- learningSpringBoot/src/main/java/com/springBootConcepts/learningSpringBoot/CourseController.java
+++ learningSpringBoot/src/main/java/com/springBootConcepts/learningSpringBoot/CourseController.java
@@ -11,9 +11,13 @@ public class CourseController {
 
     @GetMapping("/courses")
     public List<Course> retrieveAllCourses(){
+        System.out.println("Hello Kartik");
         return Arrays.asList(
                 new Course(1,"Learn React", "Kartik Jaiswal"),
-                new Course(2,"Learn Kafka", "Kartik Jaiswal")
+                new Course(2,"Learn Kafka", "Kartik Jaiswal"),
+                new Course(3,"Learn Spark", "Kartik Jaiswal"),
+                new Course(4, "Learn Java","Kartik Jaiswal")
+
         );
     }
 }
--- learningSpringBoot/src/main/java/com/springBootConcepts/learningSpringBoot/LearningSpringBootApplication.java
+++ learningSpringBoot/src/main/java/com/springBootConcepts/learningSpringBoot/LearningSpringBootApplication.java
@@ -9,6 +9,7 @@ public class LearningSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringBootApplication.class, args);
 
+
 	}
 
 }
